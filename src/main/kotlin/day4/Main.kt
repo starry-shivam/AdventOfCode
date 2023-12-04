cf2d9dd5857511da06c5390b678a6f581351abfb
starry-shivam <krshivam24@proton.me>
2023-12-04 13:51:57 +0530

    Added Day4 P2
    Signed-off-by: starry-shivam <krshivam24@proton.me>

--- src/main/kotlin/day4/Main.kt
+++ src/main/kotlin/day4/Main.kt
@@ -23,29 +23,31 @@ class Day4(private val input: List<String>) : DayInterface {
         var sum = 0
         cards.forEach { c ->
             var point = 0
-            c.scratchedNums.forEach {
-                if (it in c.winningNums) {
-                    point = if (point == 0) 1 else point * 2
-                }
-            }
+            c.scratchedNums.forEach { if (it in c.winningNums) point = if (point == 0) 1 else point * 2 }
             sum += point
         }
         return sum
     }
 
     override fun calculatePart2(): Int {
-        TODO()
+        val instances = MutableList(input.size) { 1 }
+        val cards = parseCards(input)
+        cards.forEachIndexed { i, c ->
+            val match = c.scratchedNums.count { it in c.winningNums }
+            for (j in i + 1..i + match)
+                instances[j] += instances[i]
+        }
+        return instances.sum()
     }
 
     private fun parseCards(input: List<String>): List<Card> {
-        val cards = input.map { line ->
+        return input.map { line ->
             val parts = line.split(":")
             val cardId = parts[0].trim().removePrefix("Card ").trim().toInt()
-            val numbers = parts[1].split("|").map {
-                it.trim().split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+            val numbers = parts[1].split("|").map { n ->
+                n.trim().split(" ").filter { it.isNotBlank() }.map { it.toInt() }
             }
             Card(cardId, numbers[0], numbers[1])
         }
-        return cards
     }
 }
